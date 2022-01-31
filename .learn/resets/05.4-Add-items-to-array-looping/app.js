f933809d4e271a6b6c47ec3e974cc21a20c26cb7
ErnestoXG <ggoku02@gmail.com>
2022-01-31 23:31:14 +0000

    Up to 5.4

--- .learn/resets/05.4-Add-items-to-array-looping/app.js
+++ .learn/resets/05.4-Add-items-to-array-looping/app.js
@@ -4,10 +4,4 @@ var arr = [4,5,734,43,45];
 // Your code here
 // you need to loop 10 times, for example, using a for loop
 //*****************
-let i = 0
-while(i < 10){
-    arr.push(Math.floor(Math.random()))
-    i--
-}
 
-console.log(arr);
