5ac91caa079879e9938a90330240493b02c130ce
ErnestoXG <ggoku02@gmail.com>
2022-02-03 02:50:40 +0000

    Up to 8.2

--- exercises/07.2-Letter-Counter/app.js
+++ exercises/07.2-Letter-Counter/app.js
@@ -1,6 +1,25 @@
-let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
+let par = "Hello World"
 let counts = {};
 
 // your code here
+let str = par.split(' ').join('')
+let strArr = str.split('')
+
+
+strArr.forEach(e=>{
+    let lowerE = e.toLowerCase()
+    var ocurrence = 0
+
+    for(let i=strArr.indexOf(e); i < strArr.length; i++){
+        if(lowerE == strArr[i].toLowerCase()){
+            ocurrence++
+        }
+
+        if(i == strArr.length-1){
+            counts[lowerE] = ocurrence
+        }
+    }
+})
+
 
 console.log(counts);
